cc6bf7ba26094b3b867d90454288508d75ed5584
pinyuezou-glitch <pinyuezou@gmail.com>
2026-05-01 12:00:08 -0400

    rearrangement

---
+++ scripts/Class0414/app.R
@@ -0,0 +1,56 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram (UI refers to what we gonna display)
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("VTPEH 6270"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a ggplot
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(floor(min(faithful$eruptions)), max(faithful$eruptions), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             xlab = 'Waiting time to next eruption (in mins)',
+             main = 'Histogram of waiting times')
+        
+        library("ggplot2")
+        ggplot(faithful,aes(eruptions))+
+          geom_histogram()
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
